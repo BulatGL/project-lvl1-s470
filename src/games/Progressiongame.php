5ec8ab2dc17bc29f216579545fb4f11045468404
BulatGL <galikhanovbulat@gmail.com>
2019-04-05 02:05:18 +0500

    added a new game in which you have to guess a missing number in progression

---
+++ src/games/Progressiongame.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace BrainGames\Progressiongame;
+
+use function \cli\line;
+use function \cli\prompt;
+use function BrainGames\Cli\run;
+use function BrainGames\Cli\askName;
+use function BrainGames\Cli\isUserRight;
+use function BrainGames\Cli\printQuestionAndAnswer;
+use function BrainGames\Cli\nextRoundOrFinish;
+use function BrainGames\Cli\congratulate;
+
+function makeProgression()
+{
+    $randomFirstNum = rand(1, 100);
+    $resultArray = [$randomFirstNum];
+    $randomDifference = rand(1, 10);
+    for ($i = 1; $i < 10; $i++) {
+        $resultArray[] = $resultArray[$i - 1] + $randomDifference;
+    }
+
+    return $resultArray;
+}
+
+function guessNumberInProgression()
+{
+    run("What number is missing in the progression?");
+    $name = askName();
+
+    $iterGuessNumberInProgression = function ($triesCount) use (&$iterGuessNumberInProgression, $name) {
+        if ($triesCount === 0) {
+            congratulate($name);
+            return;
+        }
+
+        $randomIndex = rand(1, 9);
+        $randomArr = makeProgression();
+        $randomArrWithMissingNum = $randomArr;
+        $rightMissingNum = $randomArrWithMissingNum[$randomIndex];
+        $randomArrWithMissingNum[$randomIndex] = '..';
+        $randomArrToString = implode(' ', $randomArrWithMissingNum);
+        $answer = printQuestionAndAnswer("Question: {$randomArrToString}");
+        nextRoundOrFinish($iterGuessNumberInProgression, isUserRight($answer, $rightMissingNum), $triesCount, $name);
+    };
+
+    $iterGuessNumberInProgression(3);
+}
